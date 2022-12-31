c813799a2998f16c62dff1194e3db6168d2ad5a9
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-12-31 07:09:46 +0000

    [Add]ログイン、ログアウト機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_parmitted_prameters, if: :devise_controller?
+
+  protected
+    def configure_parmitted_prameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
 end
