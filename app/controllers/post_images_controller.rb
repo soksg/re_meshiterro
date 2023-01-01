b2d9019ce7105ffe2c15531eef58fe28c4182e1c
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2023-01-01 12:42:16 +0000

    [Add]新規投稿機能追加

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,13 +1,33 @@
 class PostImagesController < ApplicationController
   def new
+    @post_image=PostImage.new
+  end
+
+  def create
+    # @post_image=PostImage.new(post_image_params)
+    # @post_image.user==current_user.id
+    @post_image=current_user.post_images.new(post_image_params)
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render_to request.referer
+    end
   end
 
   def index
+    @post_images=PostImage.all
   end
 
   def show
+    @post_image=PostImage.find(params[:id])
   end
 
   def destroy
+    @post_image=PostImage.find(params[:id]).destroy
   end
+
+  private
+    def post_image_params
+      params.require(:post_image).permit(:shop_name, :caption, :image)
+    end
 end
