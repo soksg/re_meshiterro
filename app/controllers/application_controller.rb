258c9ab758ea01272a6b63437d498de6ce7f2064
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2023-01-05 06:39:42 +0000

    [Add]バリデーション追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_parmitted_prameters, if: :devise_controller?
+  before_action :authenticate_user!, except: [:top]
 
   def after_sign_in_path_for(resource)
     post_images_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,9 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
 
+  # validated :name, presence: true
+  # validated :email, presence: true
+
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path=Rails.root.join('app/assets/images/no_image.jpg')
